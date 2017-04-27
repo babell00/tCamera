7dae0e268abced07f014d095fe692cd4df753c5f
Jakub Gajzler <gajzler.jakub@gmail.com>
2017-04-27 12:34:45 +0200

    working with streams

--- server/Handlers.go
+++ server/Handlers.go
@@ -8,12 +8,12 @@ import (
 	"image/jpeg"
 	"github.com/gorilla/mux"
 	"log"
-	"mime/multipart"
 	"net/textproto"
 	"bytes"
-	"fmt"
-	"sync"
+	"mime/multipart"
 	"image"
+	"sync"
+	"fmt"
 )
 
 var _service *camera.CameraService
@@ -63,36 +63,32 @@ func CameraStream(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving streaming", r.URL)
 
 	m := multipart.NewWriter(w)
-	w.Header().Set("Content-Type", "multipart/x-mixed-replace; boundary="+ m.Boundary())
+	w.Header().Set("Content-Type", "multipart/x-mixed-replace; boundary="+m.Boundary())
 	w.Header().Set("Connection", "close")
-
 	vars := mux.Vars(r)
 	path := vars["camera_path"]
 	cam := getService().GetByPath(path)
 	header := textproto.MIMEHeader{}
 
 	var buf bytes.Buffer
-	decoder ,_ := camera.Decoder(cam.MJpegUrl)
-	var mutex sync.RWMutex
-	img, _:= camera.ReadJpeg(cam.MJpegUrl)
-	go func() {
-		for {
-			var tmp image.Image
-			tmp, err := decoder.Decode()
-			if err != nil {
-				break
-			}
-			mutex.Lock()
-			img = tmp
-			mutex.Unlock()
-		}
-	}()
+	decoder, _ := camera.Decoder(cam.MJpegUrl)
 
+	var img image.Image
+	var mux sync.Mutex
 	for {
-		mutex.RLock()
+		mux.Lock()
+		tmp, err := decoder.Decode()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		img = tmp
+		mux.Unlock()
+
 		buf.Reset()
-		err := jpeg.Encode(&buf, img, nil)
-		mutex.RUnlock()
+		mux.Lock()
+		err = jpeg.Encode(&buf, img, nil)
+
 		if err != nil {
 			break
 		}
@@ -106,10 +102,10 @@ func CameraStream(w http.ResponseWriter, r *http.Request) {
 		if flusher, ok := mw.(http.Flusher); ok {
 			flusher.Flush()
 		}
+		mux.Unlock()
 	}
 
 }
-
 
 func getService() *camera.CameraService {
 	if _service == nil {
