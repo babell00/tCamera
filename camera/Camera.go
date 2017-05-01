5038821b1c51c5512a23a209d922ef76cab3a937
Jakub Gajzler <jakubg@gamehouse.com>
2017-05-01 15:17:11 +0200

    hide mjpeg internal url

--- camera/Camera.go
+++ camera/Camera.go
@@ -7,7 +7,7 @@ import (
 type Camera struct {
 	Name   string `json:"name"`
 	Id     string `json:"id"`
-	MJpegUrl    string `json:"mjpeg_url"`
+	MJpegUrl    string `json:"-"`
 	PublicImageUrl string `json:"public_image_url"`
 	PublicStreamUrl string `json:"-"`
 	UrlPath   string `json:"url_path"`
